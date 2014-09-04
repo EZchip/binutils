cd5d208c4ae7fff4adffe955d37584414893902d
Roy Vardi <royv@ezchip.com>
2014-09-04 08:28:14 +0300

    [binutils-nps] Adding misc. changes
    Add new format to bdfre, add m flag
    Fix adadd{ l} format size from 2 --> 4
    Fix cst size from 4 to 8 bytes
    adadd{ l} virtual addresses target size changes
    Add new cgi instruction
    Replace aric.r virtual formats
    Turn on C flag in aril command format
    Set size==4 (0x0) for adadd
    Fix aric format entry size
    Remove redundant duplication of bits for cp flavour
    using additional register for cp.e command flavours
    Copy src2 into src1 in opcode 0xA commands
    Fix dma cp instruction decoding
    Fix dma cp instruction format decoding
    Copy src1 into src2 for dma command
    Fix dma instruction remove redundant src1->src2 copy
    Copy src1 into src2 for dma command format without src2
    Copy src1 into src2 for dma command formats without src2
    Turn off .na bit (27) in cp instruction
    Copy src1->src2 and vise versa for dma commands
    Fix dma cp instruction disassembly
    Remove dst register from bdfre,sbdfre,idxfre,sidxfre
    Limit lkpetcm keysize valid values
    Fix dma instruction decoding
    Don't turn on bit 27 (.na flag) in dma cp instruction
    Turn off .na flag for another cp instruction
    Fix dma cp instruction disassembly
    Turn off bit 12 in aric command (entry size)
    Update mmnt limm fields according to ACC 3.4
    Add verbosity to error messages
    Remove idxbgt instruction
    mmnt - copy src1 into src2 when src2 is not given
    new formats for schd command: ie1,ie2,ie12
    Fix broken disassembly
    Fix cgi command flavour - check flag
    Add new aric virtual format
    Add new xstl format (xstl.di SRC1,[mbd:SRC2])
    
    Signed-off-by: Roy Vardi <royv@ezchip.com>

--- bfd/version.h
+++ bfd/version.h
@@ -1,5 +1,5 @@
 #define BFD_VERSION_DATE 20130325
 #define BFD_VERSION @bfd_version@
-#define BFD_EZCHIP_EXTRA_VERSION ".EZchip_2.0"
+#define BFD_EZCHIP_EXTRA_VERSION ".EZchip_2.1"
 #define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ BFD_EZCHIP_EXTRA_VERSION
 #define REPORT_BUGS_TO @report_bugs_to@
